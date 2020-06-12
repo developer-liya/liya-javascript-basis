7cc1f09ffdfcbd6eaa8eac2edccf9863c783a5b7
liyaqd <liya341221@163.com>
2020-06-12 09:50:28 +0800

    修改引入方式

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 
 // 引入es2020语法
-import ES2020 from './js/es10/index';
+// import ES2020 from './js/es2020/index';
+// import ES2019 from './js/es2019/index';
+// import ES2018 from './js/es2018/index';
+// import ES2017 from './js/es2017/index';
+import ES2016 from './js/es2016/index';
 
-ES2020();
+// ES2020();
+// ES2019()
+// ES2018()
+// ES2017()
+ES2016()
